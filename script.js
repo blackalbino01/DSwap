77b19bbb104e13d13ba7919628db874ba6676316
Hussaini Muhammad Auwal <hauwal4969@gmail.com>
2022-01-27 00:51:54 +0100

    fixing bug

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const Swap = artifacts.require("Swap");
-const SwapRouter = artifacts.require("SwapRouter");
+const SwapRouter = require("@uniswap/v3-periphery/artifacts/contracts/SwapRouter.sol/SwapRouter.json");
 const IERC20 = require("@openzeppelin/contracts/build/contracts/IERC20.json");
 
 
@@ -9,21 +9,24 @@ module.exports = async function(callback) {
 	try{
 		const accounts = await web3.eth.getAccounts();
 
-		const owner = accounts[0]
+		const owner = accounts[1]
 
-		const trader = accounts[1]
-		const swap = await Swap.new('0xE592427A0AEce92De3Edee1F18E0157C05861564', {from: owner});
-		const swapRouter = await SwapRouter.new('0x1F98431c8aD98523631AE4a59f267346ea31F984', '0xc778417E063141139Fce010982780140Aa0cD5Ab',{from: owner});
+		const trader = accounts[0]
+
+
+		const router = new web3.eth.Contract(SwapRouter.abi, '0xE592427A0AEce92De3Edee1F18E0157C05861564', {from: owner, data: SwapRouter.bytecode});
 		
+		const swapTokens = await Swap.new(router.options.address, {from: owner});
+
 		const DAI = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
 		const WETH9 = '0xc778417E063141139Fce010982780140Aa0cD5Ab';
-		const amountIn = web3.utils.toWei('0.4', 'ether');
+		const amountIn = web3.utils.toWei('0.1', 'ether');
 
-		const token = new web3.eth.Contract(IERC20.abi, WETH9);
+		const token = new web3.eth.Contract(IERC20.abi, WETH9, {from: owner});
 
-		await token.methods.approve(swap.address, amountIn).send({ from: owner});
+		await token.methods.approve(swapTokens.address, amountIn).send({ from: trader});
 
-		const test = await swap.swap(WETH9, DAI, amountIn, {from: owner});
+		const test = await swapTokens.swap(DAI, WETH9, amountIn, {from: trader});
 		console.log(test);
 	}
 
